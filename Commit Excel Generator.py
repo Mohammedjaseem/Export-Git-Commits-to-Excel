3f064d1f002ff95eab95c449bf92a020d96b08c6
Mohammed Jaseem Tp <mail@jassy.in>
2023-05-30 15:41:40 +0530

    Added Commit Dates

--- Commit Excel Generator.py
+++ Commit Excel Generator.py
@@ -15,32 +15,46 @@ headers = {
     'Accept': 'application/vnd.github.v3+json'
 }
 
-# Send the GET request to retrieve all commits
-response = requests.get(url, headers=headers)
+# Create an empty list to store all commits
+all_commits = []
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Extract the commit data from the response
-    commits = response.json()
+# Send a GET request to retrieve commits page by page
+page = 1
+while True:
+    # Add pagination information to the API endpoint
+    page_url = f'{url}?page={page}&per_page=100'
     
-    # Create a list to store the commit details
-    commit_details = []
+    # Send the GET request to retrieve a page of commits
+    response = requests.get(page_url, headers=headers)
     
-    # Iterate through each commit
-    for commit in commits:
-        sha = commit['sha']
-        author = commit['commit']['author']['name']
-        message = commit['commit']['message']
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Extract the commit data from the response
+        commits = response.json()
         
-        # Add the commit details to the list
-        commit_details.append({'SHA': sha, 'Author': author, 'Message': message})
-    
-    # Create a pandas DataFrame from the commit details
-    df = pd.DataFrame(commit_details)
-    
-    # Save the DataFrame to an Excel file
-    df.to_excel('commit_details.xlsx', index=False)
-    
-    print('Commit details saved to commit_details.xlsx')
-else:
-    print(f'Request failed with status code {response.status_code}')
+        # Check if the current page has any commits
+        if len(commits) == 0:
+            break  # Exit the loop if there are no more commits
+        
+        # Iterate through each commit
+        for commit in commits:
+            sha = commit['sha']
+            author = commit['commit']['author']['name']
+            message = commit['commit']['message']
+            date = commit['commit']['author']['date']
+            
+            # Add the commit details to the list
+            all_commits.append({'SHA': sha, 'Author': author, 'Message': message, 'Date': date})
+        
+        page += 1
+    else:
+        print(f'Request failed with status code {response.status_code}')
+        break
+
+# Create a pandas DataFrame from the commit details
+df = pd.DataFrame(all_commits)
+
+# Save the DataFrame to an Excel file
+df.to_excel('commit_details.xlsx', index=False)
+
+print('Commit details saved to commit_details.xlsx')
